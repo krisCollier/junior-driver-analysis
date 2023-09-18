511e4f62b700905b4def31da787bc7b4050a1641
KwispyOnTheBigBoi <kc4g20@soton.ac.uk>
2023-09-18 20:02:59 +0100

    Updated to include results etc

--- SQL Scripts/create-database.sql
+++ SQL Scripts/create-database.sql
@@ -25,21 +25,6 @@ CREATE TABLE seasons (
   PRIMARY KEY (year),
 );
 
-CREATE TABLE races (
-  raceId int NOT NULL IDENTITY(1,1),
-  year int NOT NULL DEFAULT '0',
-  round int NOT NULL DEFAULT '0',
-  circuitId int NOT NULL DEFAULT '0',
-  name varchar(255) NOT NULL DEFAULT '',
-  date date NOT NULL DEFAULT '0000-00-00',
-  url varchar(255) DEFAULT NULL,
-  p_time time DEFAULT NULL,
-  q_time time DEFAULT NULL,
-  sr_time time DEFAULT NULL,
-  r_time time DEFAULT NULL,
-  PRIMARY KEY (raceId),
-);
-
 CREATE TABLE circuits (
   circuitId int NOT NULL IDENTITY(1,1),
   circuitRef varchar(255) NOT NULL DEFAULT '',
@@ -53,6 +38,34 @@ CREATE TABLE circuits (
   PRIMARY KEY (circuitId),
 );
 
+CREATE TABLE races (
+  raceId int NOT NULL IDENTITY(1,1),
+  year int NOT NULL DEFAULT '0',
+  round int NOT NULL DEFAULT '0',
+  circuitId int NOT NULL DEFAULT '0',
+  name varchar(255) NOT NULL DEFAULT '',
+  date date NOT NULL DEFAULT '0000-00-00',
+  url varchar(255) DEFAULT NULL,
+  PRIMARY KEY (raceId),
+  FOREIGN KEY (circuitId) REFERENCES circuits(circuitId)
+);
+
+CREATE TABLE sessionTypes (
+	sessionTypeId int NOT NULL IDENTITY(1,1),
+	sessionName varchar(255) NOT NULL DEFAULT '',
+	PRIMARY KEY (sessionTypeId),
+);
+
+CREATE TABLE sessions (
+	sessionId int NOT NULL IDENTITY(1,1),
+	raceId int NOT NULL,
+	sessionTypeId int NOT NULL,
+	start_time time DEFAULT NULL,
+	PRIMARY KEY (sessionId),
+	FOREIGN KEY (sessionTypeId) REFERENCES sessionTypes(sessionTypeId),
+	FOREIGN KEY (raceId) REFERENCES races(raceId),
+);
+
 CREATE TABLE status (
   statusId int NOT NULL IDENTITY(1,1),
   status varchar(255) NOT NULL DEFAULT '',
@@ -60,12 +73,53 @@ CREATE TABLE status (
 );
 
 CREATE TABLE lapTimes (
-  raceId int NOT NULL,
+  sessionId int NOT NULL,
   driverId int NOT NULL,
   lap int NOT NULL,
-  seconds int DEFAULT NULL,
+  seconds float DEFAULT NULL,
   sess_time int DEFAULT NULL,
-  time_set date DEFAULT NULL,
-  PRIMARY KEY (raceId, driverId, lap),
-  FOREIGN KEY (raceId) REFERENCES races(raceId)
+  time_set time DEFAULT NULL,
+  s1_time float DEFAULT NULL,
+  s1_speed float DEFAULT NULL,
+  s2_time float DEFAULT NULL,
+  s2_speed float DEFAULT NULL,
+  s3_time float DEFAULT NULL,
+  s3_speed float DEFAULT NULL,
+  position int DEFAULT NULL,
+  gap float DEFAULT NULL,
+  PRIMARY KEY (driverId, sessionId, lap),
+  FOREIGN KEY (driverId) REFERENCES drivers(driverId),
+  FOREIGN KEY (sessionId) REFERENCES sessions(sessionId),
+);
+
+CREATE TABLE pitStops (
+  sessionId int NOT NULL,
+  driverId int NOT NULL,
+  stop int NOT NULL,
+  lap int NOT NULL,
+  time time NOT NULL,
+  duration float DEFAULT NULL,
+  PRIMARY KEY (sessionId, driverId, stop),
+  FOREIGN KEY (driverId) REFERENCES drivers(driverId),
+  FOREIGN KEY (sessionId) REFERENCES sessions(sessionId),
+);
+
+CREATE TABLE results (
+  resultId int NOT NULL IDENTITY(1,1),
+  sessionId int NOT NULL,
+  driverId int NOT NULL DEFAULT '0',
+  constructorId int NOT NULL DEFAULT '0',
+  number int NOT NULL DEFAULT '0',
+  grid int NOT NULL DEFAULT '0',
+  position int DEFAULT NULL,
+  positionOrder int NOT NULL DEFAULT '0',
+  points float NOT NULL DEFAULT '0',
+  laps int NOT NULL DEFAULT '0',
+  time float DEFAULT NULL,
+  fastestLapTime float DEFAULT NULL,
+  fastestLapSpeed float DEFAULT NULL,
+  statusId int NOT NULL DEFAULT '0',
+  PRIMARY KEY (sessionId, driverId),
+  FOREIGN KEY (sessionId) REFERENCES sessions(sessionId),
+  FOREIGN KEY (driverId) REFERENCES drivers(driverId),
 );
